de12f448095dd5a11fa877b4fe5a26d767161bae
ProseNVerse <walker.atlas@yahoo.com>
2018-05-04 10:23:18 +0100

    minesweeper basic

---
+++ Minesweeper/src/Minesweeper/Grid.java
@@ -0,0 +1,10 @@
+package Minesweeper;
+
+public class Grid {
+	
+	int [] XCoor = {0,1,2,3,4,5,6,7,8,9};
+	int [] YCoor = {0,1,2,3,4,5,6,7,8,9};
+	int XL = XCoor.length;
+	int YL = YCoor.length;
+
+}
---
+++ Minesweeper/src/Minesweeper/Main.java
@@ -0,0 +1,24 @@
+package Minesweeper;
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+
+public class Main {
+	
+	static boolean Bomb = false;
+	private static String answer = "";
+	private static String reply = "";
+	private static Scanner keyboard = new Scanner (System.in);
+	static int A = (int)(Math.random()*9);
+	static int B = (int)(Math.random()*9);
+	static String Bomb1 = ("("+A + ", " + B+")");
+	
+
+	public static void main(String[] args) {
+	
+		
+	}
+
+}
