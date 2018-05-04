77d70734ef50a809806ab31bf4e50efd3c0cd33b
ProseNVerse <walker.atlas@yahoo.com>
2018-05-04 13:27:23 +0100

    final commit

--- MinesweeperGUI/src/GUI/MSButton.java
+++ MinesweeperGUI/src/GUI/MSButton.java
@@ -5,12 +5,27 @@ import javax.swing.JButton;
 public class MSButton extends JButton {
 	private static final long serialVersionUID = 1L;
 	boolean hasBomb;
+	String numBombs;
 	
 	public MSButton (boolean bomb) {
 		super();
 		this.hasBomb = bomb;
+		numBombs = "B";
 	}
 	
+	public boolean getBomb () {
+		return this.hasBomb;
+	}
 	
+	public void setBomb (boolean arg) {
+		this.hasBomb = arg;
+	}
 	
+	public String getNumBombs() {
+		return this.numBombs;
+	}
+	
+	public void setNumBombs(String arg) {
+		this.numBombs = arg;
+	}
 }
--- MinesweeperGUI/src/GUI/MinesweeperWindow.java
+++ MinesweeperGUI/src/GUI/MinesweeperWindow.java
@@ -12,18 +12,18 @@ import java.lang.String;
 
 import GUI.MSButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MinesweeperWindow {
-	
+
 	public JFrame frame = new JFrame();
 	public JPanel panel = new JPanel();
-	
-	
+
 	private MSButton[][] mineField;
+	
 	int mMines = 100; // number of mines
 
-	
-	
 	/**
 	 * @wbp.parser.entryPoint
 	 */
@@ -39,14 +39,11 @@ public class MinesweeperWindow {
 			}
 		});
 	}
-	
-	
-	
+
 	public MinesweeperWindow() {
 		initialize();
 	}
 
-	
 	private void initialize() {
 		/*
 		 * Create a frame and call a function to build panels for different layouts
@@ -56,7 +53,7 @@ public class MinesweeperWindow {
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
 		
-		panel.setBounds(0, 0, 361, 361); //39
+		panel.setBounds(10, 0, 368, 361); //39
 		panel.setLayout(null);
 		panel.setVisible(true);
 		frame.getContentPane().add(panel);
@@ -65,76 +62,83 @@ public class MinesweeperWindow {
 		
 	}
 
-
-
-//	public ArrayList<ArrayList<JButton>> getmineField() {
-//		return mineField;
-//	}
-
-
-
 	public void setmineField(int buttonSize, int gridSize) {
 		mineField = new MSButton[gridSize][gridSize];
-		
-		for(int i = 0; i < gridSize; i++) {
-			for(int j = 0; j < gridSize; j++) {
+
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
 				mineField[i][j] = new MSButton(true);
-				mineField[i][j].setSize(buttonSize,buttonSize);
+				mineField[i][j].setSize(buttonSize, buttonSize);
 				mineField[i][j].setLocation(
-						(panel.getWidth() - buttonSize*gridSize + buttonSize)/2 + 
-						i*buttonSize,j*buttonSize+40);
-				mineField[i][j].setBackground(new Color(238 ,238,238));
+						(panel.getWidth() - buttonSize * gridSize + buttonSize) / 2 + i * buttonSize,
+						j * buttonSize + 40);
+				mineField[i][j].setBackground(new Color(238, 238, 238));
 				mineField[i][j].setVisible(true);
+
+				mineField[i][j].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						// The main GUI button feature - the click, to be used here unsparingly
+						mineField[i][j].getBomb();
+					}
+				});
+
+
 				this.panel.add(mineField[i][j]);
+			}
+		}
+		
+		this.setMines(gridSize);
+		this.setHiddenText(gridSize);
+	}
+
+	public int mineAt(int i, int j) {
+		int mHeight = 0;
+		int mWidth = 0;
+		if (j >= 0 && j < mHeight && i >= 0 && i < mWidth) {
+			if (mineField[i][j].getBomb() == true) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else {
+			return 0;
+		}
+	}
+
+	public void setMines(int gridSize) {
+		Random rand = new Random();
+		int mineCount = 0;
+		while (mineCount < 30) {
+			int randomInteger = (int) (rand.nextInt(gridSize));
+			int randomInteger2 = (int) (rand.nextInt(gridSize));
+			if (mineField[randomInteger][randomInteger2].getBomb() == false) {
+				mineField[randomInteger][randomInteger2].setBomb(true);
+				mineCount++;
+				
 			}
 		}
 	}
 	
-	public int mineAt(int i, int j) {
-		int mHeight = 0;
-			int mWidth = 0;
-		    if(j >= 0 && j < mHeight && i >= 0 && i < mWidth) {
-		      return 1;
-		    } else {
-		      return 0;
-		    }
-		  }
-	
-	public char minesNear(int i, int j) {
-		int mines = 0;
-		// check mines in all directions
-		mines += mineAt(i - 1, j - 1);  // NW
-		mines += mineAt(i - 1, j);      // N
-		mines += mineAt(i - 1, j + 1);  // NE
-		mines += mineAt(i, j - 1);      // W
-		mines += mineAt(i, j + 1);      // E
-		mines += mineAt(i + 1, j - 1);  // SW
-		mines += mineAt(i + 1, j);      // S
-		mines += mineAt(i + 1, j + 1);  // SE
-		   if(mines > 0) {
-		      return (char)(mines + 48);
-		   		} else {
-				    return ' ';
+	public void setHiddenText(int gridSize) {
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
+				if (mineField[i][j].getBomb() == true) {
+					mineField[i][j].setText("B");
+				} else {
+					int mines = 0;
+					mines += mineAt(i - 1, j - 1);
+					mines += mineAt(i	 , j - 1);
+					mines += mineAt(i + 1, j - 1);
+					mines += mineAt(i - 1, j    );
+					mines += mineAt(i + 1, j    );
+					mines += mineAt(i - 1, j + 1);
+					mines += mineAt(i    , j + 1);
+					mines += mineAt(i + 1, j + 1);
+					
+					mineField[i][j].setText(Integer.toString(mines));
 				}
 			}
-		
+		}
+	}
 
-public static void mines(MSButton[][] buttons)
-{
-    Random rand = new Random();
-    int mineCount = 0;
-    while (mineCount < 30)
-    {
-        int randomInteger = (int) (rand.nextDouble() * buttons.length);
-        int randomInteger2 = (int) (rand.nextDouble() * buttons[0].length);
-        if (buttons[randomInteger][randomInteger2].getText().equals("X"))
-            continue;
-        else
-        {
-            buttons[randomInteger][randomInteger2].setText("X");
-            mineCount++;
-        }
-    }
-
-}
 }
